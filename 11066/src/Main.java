0ac17b8b229eb6c47d597d865b4bfef158910554
Ksieon <keumsiun0503@gmail.com>
2025-03-02 23:35:49 +0900

    250302 solved 1003

--- 11066/src/Main.java
+++ 11066/src/Main.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class Main {
     static int T;
     static int K;
-    static PriorityQueue<Integer> pq = new PriorityQueue<>();
+    static Queue<Integer> queue = new LinkedList<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
@@ -16,15 +16,35 @@ public class Main {
             int result = 0;
             StringTokenizer st = new StringTokenizer(br.readLine());
             while(K-->0){
-                pq.add(Integer.parseInt(st.nextToken()));
+                queue.add(Integer.parseInt(st.nextToken()));
             }
-            while(pq.size()!=1){
-                int X = pq.poll() + pq.poll();
-                result+=X;
-                pq.add(X);
+            int minSize = Integer.MAX_VALUE;
+            int minIndex = 0;
+            while(queue.size()!=1){
+                for(int i=0; i<queue.size()-1; i++){
+                    int A = queue.poll();
+                    int B = queue.poll();
+                    int X = A + B;
+                    if(X<minSize){
+                        minSize = X;
+                        minIndex = i;
+                    }
+                    queue.add(A);
+                    queue.add(B);
+                }
+                for(int i=0; i<queue.size()-2; i++){
+                    if(i==minIndex){
+                        int X = queue.poll() + queue.poll();
+                        result+=X;
+                        queue.add(X);
+                    } else {
+                        int C = queue.poll();
+                        queue.add(C);
+                    }
+                }
             }
             sb.append(result).append('\n');
-            pq.clear();
+            queue.clear();
         }
         br.close();
         System.out.print(sb.toString());
--- 1520/src/Main.java
+++ 1520/src/Main.java
@@ -2,28 +2,26 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int M, N, countCourse = 0;
+    static int M, N;
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
-    static int[][] map;
-    static boolean[][] dp;
-    static void DFS(int x, int y){
+    static int[][] map, dp;
+    static int DFS(int x, int y){
         if(x==M-1 && y==N-1){
-            countCourse++;
-            return;
+            return 1;
         }
-        boolean canMove = false;
+        if(dp[x][y] != -1){
+            return dp[x][y];
+        }
+        dp[x][y] = 0;
         for(int i=0; i<4; i++){
             int nextX = dx[i] + x;
             int nextY = dy[i] + y;
-            if(nextX>=0 && nextX<M && nextY>=0 && nextY<N && (map[x][y]>map[nextX][nextY]) && !dp[nextX][nextY]){
-                canMove = true;
-                DFS(nextX, nextY);
+            if(nextX>=0 && nextX<M && nextY>=0 && nextY<N && (map[x][y]>map[nextX][nextY])){
+                dp[x][y] += DFS(nextX, nextY);
             }
         }
-        if(!canMove){
-            dp[x][y] = true;
-        }
+        return dp[x][y];
     }   
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -34,16 +32,22 @@ public class Main {
         M = Integer.parseInt(st.nextToken());
         N = Integer.parseInt(st.nextToken());
         map = new int[M][N];
-        dp = new boolean[M][N];
+        dp = new int[M][N];
 
         for(int i=0; i<M; i++){
             st = new StringTokenizer(br.readLine());
             for(int j=0; j<N; j++){
                 map[i][j] = Integer.parseInt(st.nextToken());
+                dp[i][j] = -1;
             }
         }
-        dp[0][0] = true;
-        DFS(0, 0);
-        System.out.println(countCourse);
+        int result = DFS(0, 0);
+        for(int i=0; i<M; i++){
+            for(int j=0; j<N; j++){
+                System.out.print(dp[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println(result);
     }
 }

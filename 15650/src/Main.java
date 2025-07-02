10cd71f3f9c23514f2afc8d582be98a1dcbcc758
Ksieon <keumsiun0503@gmail.com>
2025-07-03 00:23:53 +0900

    :recycle: Refactor: 250703 update 15650

--- 15650/src/Main.java
+++ 15650/src/Main.java
@@ -5,18 +5,22 @@ public class Main {
     static int N, M;
     static Stack<Integer> stack = new Stack<>();
     static StringBuilder sb = new StringBuilder();
+    static boolean[] visited;
     static void DFS(int num){
-        stack.push(num);
 
         if(stack.size() == M){
             for(int i=0; i<M; i++){
                 sb.append(stack.get(i)).append(" ");
             }
             sb.append("\n");
+            return;
         }
         for(int i=num+1; i<N+1; i++){
-            if(!stack.contains(i)){
+            if(!visited[i]){
+                visited[i] = true;
+                stack.push(i);
                 DFS(i);
+                visited[i] = false;
                 stack.pop();
             }
         }
@@ -28,11 +32,10 @@ public class Main {
         
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        
-        for(int i=1; i<N+1; i++){
-            DFS(i);
-            stack.removeAllElements();
-        }
+
+        visited = new boolean[N+1];
+
+        DFS(0);
         System.out.println(sb.toString());
     }
 }

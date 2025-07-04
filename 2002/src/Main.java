fcb8d1d4ad51ac7cd301b2c156a4d5a42ad042bd
Ksieon <keumsiun0503@gmail.com>
2025-07-04 11:53:40 +0900

    :sparkles: Feat: 250704 solved 2002

--- 2002/src/Main.java
+++ 2002/src/Main.java
@@ -3,19 +3,32 @@ import java.util.*;
 
 public class Main {
     static int N, count=0;
-    static HashMap<String, Integer> hash = new HashMap<>();
+    static HashMap<String, Integer> hashmap = new HashMap<>();
+    static HashSet<String> hashset = new HashSet<>();
+    static String[] list;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         N = Integer.parseInt(br.readLine());
+        list = new String[N];
 
         for(int i=0; i<N; i++){
-            hash.put(br.readLine(), i);
+            String carNum = br.readLine();
+            hashmap.put(carNum, i);
         }
 
         for(int i=0; i<N; i++){
-            if(hash.get(br.readLine()) < i){
-                count++;
+            String carNum = br.readLine();
+            list[i] = carNum;
+
+            int originOrder = hashmap.get(carNum);
+            if(originOrder < i){
+                for(int j=0; j<i; j++){
+                    if(!hashset.contains(list[j]) && hashmap.get(list[j]) > originOrder){
+                        hashset.add(list[j]);
+                        count++;
+                    }
+                }
             }
         }
         System.out.println(count);

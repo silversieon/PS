e758e85bebb0f850c6dbc732817b0a95981e9ecb
Sieon Keum <keumsiun0503@gmail.com>
2025-08-03 13:40:31 +0900

    :wrench: Not solved: 250803

--- 2579/src/Main.java
+++ 2579/src/Main.java
@@ -2,16 +2,23 @@ import java.io.*;
 
 public class Main {
     static int N;
-    static int[] score, dp;
+    static int[] score;
+    static int[][] dp;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         N = Integer.parseInt(br.readLine());
-        score = new int[N+3];
-        dp = new int[N+3];
-        for(int i=3; i<N+3; i++){
+        score = new int[N+2];
+        dp = new int[N+2][2];
+        for(int i=2; i<N+2; i++){
             score[i] = Integer.parseInt(br.readLine());
         }
-        System.out.println(dp[N+2]);
+        for(int i=2; i<N+2; i++){
+            dp[i][0] = score[i] + dp[i-1][1];
+            dp[i][1] = score[i] + dp[i-1][0];
+        }
+
+        int result = Math.max(dp[N+1][0], dp[N+1][1]);
+        System.out.println(result);
     }
 }

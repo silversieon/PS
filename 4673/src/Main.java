3c2354a2d3c0df1b2365f34568028c28212a18e4
Sieon Keum <keumsiun0503@gmail.com>
2025-08-29 14:49:53 +0900

    :recycle: Refactor: 250829 solved 4673

--- 4673/src/Main.java
+++ 4673/src/Main.java
@@ -1,59 +1,20 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    static final int MAX = 10000;
-    static int[] non_self = new int[MAX+1];
-    static int count_non_self = 0;
-    static boolean first_check = true;
-    static int recursive(int i){
-        if(first_check){
-            if(i/10<1){
-                return i+i;
-            }
-        }
-        else{
-            if(i/10<1){
-                return i;
-            }
-        }
-        int result = 0;
-        if(first_check){
-            result = i;
-            first_check = false;
-        }
-        result+=i%10;
-        int num = (int)(i/10);
-        result+=recursive(num);
-        return result;
-    }
-    static boolean find_non_self(int num){
-        for(int i=1; i<=MAX; i++){
-            if(non_self[i]==num){
-                return true;
-            }
-            else{
-                continue;
-            }
-        }
-        return false;
-    }
+    static HashSet<Integer> hashSet = new HashSet<>();
     public static void main(String[] args) throws IOException {
-        for(int i=1; i<=MAX; i++){
-            int num = recursive(i);
-            if(num<=MAX){
-                non_self[i] = num;
-                first_check = true; 
+        for(int i=1; ; i++){ 
+            String result = String.valueOf(i);
+            int tmp = Integer.parseInt(result);
+            for(int k=0; k<result.length(); k++){
+                tmp += Integer.parseInt(result.substring(k, k+1));
             }
-            else{
-                break;
-            }
+            if(tmp>10035) break;
+            hashSet.add(tmp);
         }
-        for(int i=1; i<=MAX; i++){
-            if(find_non_self(i)){
-                continue;
-            }
-            else{
+        for(int i=1; i<=10000; i++){
+            if(!hashSet.contains(i)){
                 System.out.println(i);
             }
         }

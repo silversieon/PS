e75fd561a6b486f83561e7c9da10c6ee6eff4c6f
Ksieon <keumsiun0503@gmail.com>
2025-09-02 00:26:23 +0900

    :sparkles: Feat: 250902 solved 1629

--- 1629/src/Main.java
+++ 1629/src/Main.java
@@ -2,7 +2,21 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int A, B, C;
+    static long A, B, C, result = 1;
+    static void calc(long k){
+        if(k==1){
+            result = (result * A) % C;
+            return;
+        } 
+        if(k%2==1){
+            result = (result * A) % C;
+            k-=1;
+            calc(k);
+        } else {
+            A= (A*A) % C;
+            calc(k/2);
+        }
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -11,13 +25,7 @@ public class Main {
         B = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
 
-        int result = 1;
-        String binary = Integer.toBinaryString(B);
-        A%=C;
-        for(int i=0; i<binary.length(); i++){
-            if(binary.substring(i, i+1).equals("0")) continue;
-            result = (result % C * (int)Math.pow(A, (int)Math.pow(2, binary.length()- i - 1)) % C) % C;
-        }
-        System.out.println(result%C);
+        calc(B);
+        System.out.println(result);
     }
 }

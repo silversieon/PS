0d0c333557bdbeaea4f1b75da14c43e6006bd369
Sieon Keum <keumsiun0503@gmail.com>
2025-09-05 17:26:05 +0900

    [Silver V] Title: 국회의원 선거, Time: 104 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Silver/1417. 국회의원 선거/국회의원 선거.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, dasom, count = 0;
+    static PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        dasom = Integer.parseInt(br.readLine());
+        while(N-->1){
+            int a = Integer.parseInt(br.readLine());
+            pq.add(a);
+        }
+        
+        if(!pq.isEmpty()){
+            while(dasom<=pq.peek()){
+                dasom+=1;
+                pq.add(pq.poll()-1);
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}

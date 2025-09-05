1e885774c126bbcfc8d3e4e299e07ec840ee3e8e
Sieon Keum <keumsiun0503@gmail.com>
2025-09-05 17:17:21 +0900

    [Gold II] Title: 보석 도둑, Time: 1736 ms, Memory: 136440 KB -BaekjoonHub

---
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, K, count = 0;
+    static long result = 0;
+    static Integer[] C;
+    static Integer[][] jewelry;
+    static PriorityQueue<Integer[]> priorityJewelryWeight = new PriorityQueue<>(Comparator.comparingInt((Integer[] a)->a[0]));
+    static PriorityQueue<Integer[]> priorityJewelryValue = new PriorityQueue<>(Comparator.comparingInt((Integer[] a)->a[1]).reversed());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        jewelry = new Integer[N][2];
+        C = new Integer[K];
+
+        for(int i=0; i<N; i++){
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            priorityJewelryWeight.add(new Integer[]{x, y});
+        }
+        
+        for(int i=0; i<K; i++){
+            C[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(C);
+        
+        for(int i=0; i<K; i++){
+            while(!priorityJewelryWeight.isEmpty()){
+                if(priorityJewelryWeight.peek()[0] > C[i]){
+                    break;
+                }
+                priorityJewelryValue.add(priorityJewelryWeight.poll());
+            }
+            if(!priorityJewelryValue.isEmpty()){
+                result+=priorityJewelryValue.poll()[1];
+            }
+        }
+        System.out.println(result);
+    }
+}

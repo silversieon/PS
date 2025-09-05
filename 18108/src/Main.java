440d1482f9ec7db155f824b264b01cd3e951c3f3
Sieon Keum <keumsiun0503@gmail.com>
2025-09-05 18:44:36 +0900

    :recycle: Refactor: clear

--- 18108/src/Main.java
+++
@@ -1,9 +0,0 @@
-import java.io.*;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        System.out.println(Integer.parseInt(br.readLine())-543);
-    }
-}

14657365a713e62a8be9f2602f17dd9c78b5b73f
Sieon Keum <keumsiun0503@gmail.com>
2025-09-05 17:39:17 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14664 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -0,0 +1,49 @@
+import java.awt.Point;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+class Main{
+    static int N;
+    static int M;
+    static int[][] arr;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static Queue<Point> queue = new LinkedList<>();
+    static int BFS(int x, int y){
+        queue.offer(new Point(x, y));
+        while(!queue.isEmpty()){
+            Point currentPoint = queue.poll();
+            int currentX = (int)(currentPoint.getX());
+            int currentY = (int)(currentPoint.getY());
+            for(int i=0; i<4; i++){
+                int nextX = currentX + dx[i];
+                int nextY = currentY + dy[i];
+                if(nextX>=0 && nextX<N && nextY>=0 && nextY<M && arr[nextX][nextY]==1){
+                    queue.offer(new Point(nextX, nextY));
+                    arr[nextX][nextY] = arr[currentX][currentY] + 1;
+                }
+            }
+        }
+        return arr[N-1][M-1];
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N][M];
+
+        for(int i=0; i<N; i++){
+            String str = br.readLine();
+            for(int j=0; j<M; j++){
+                arr[i][j] = str.charAt(j) - '0';
+            }
+        }
+
+        System.out.println(BFS(0, 0));
+    }
+}

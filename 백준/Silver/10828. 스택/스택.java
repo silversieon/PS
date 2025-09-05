af7f19feb3a8d1f3348a86aa03d4e84cf85bd052
Sieon Keum <keumsiun0503@gmail.com>
2025-09-05 18:09:04 +0900

    [Silver IV] Title: 스택, Time: 156 ms, Memory: 19628 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        Stack<Integer> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        String cmd;
+        for(int i=0; i<N; i++){
+            st = new StringTokenizer(br.readLine());
+            cmd = st.nextToken();
+            switch(cmd){
+                case "push" :
+                    int x = Integer.parseInt(st.nextToken());
+                    stack.push(x);
+                    break;
+                case "pop" :
+                    if(stack.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    }
+                    else{
+                        sb.append(stack.pop()).append("\n");
+                    }
+                    break;
+                case "size" :
+                    sb.append(stack.size()).append("\n");
+                    break;
+                case "empty" :
+                    if(stack.isEmpty()) {
+                        sb.append(1).append("\n");
+                    }
+                    else{
+                        sb.append(0).append("\n");
+                    }
+                    break;
+                case "top" :
+                    if(stack.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    }
+                    else{
+                        sb.append(stack.peek()).append("\n");
+                    }
+                    break;
+            }
+        }
+        br.close();
+        System.out.print(sb.toString());
+    }
+}

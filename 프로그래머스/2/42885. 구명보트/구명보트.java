4238342dd00bde190dad483b787acd78a86eef5d
Sieon Keum <keumsiun0503@gmail.com>
2025-09-25 02:37:38 +0900

    [level 2] Title: 구명보트, Time: 11.18 ms, Memory: 58.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        int min = 0;
+        Arrays.sort(people);
+        for(int i=people.length-1; i>=min; i--){
+            if(people[i] + people[min] <= limit) min++;
+            answer++;
+        }
+        return answer;
+    }
+}

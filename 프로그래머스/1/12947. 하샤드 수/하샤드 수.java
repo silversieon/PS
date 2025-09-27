aab1a78d4f6bfadf2d582b6879e05ffe92f2f9ed
Sieon Keum <keumsiun0503@gmail.com>
2025-09-27 15:05:14 +0900

    [level 1] Title: 하샤드 수, Time: 0.07 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean solution(int x) {
+        String s = String.valueOf(x);
+        int sum = 0;
+        for(int i=0; i<s.length(); i++){
+            sum+=Integer.parseInt(s.substring(i, i+1));
+        }
+        return x%sum==0;
+    }
+}

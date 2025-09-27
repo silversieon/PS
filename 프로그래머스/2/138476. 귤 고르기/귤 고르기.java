4fb231e2655cbc3537b005f6de7ca9f65ab48eab
Sieon Keum <keumsiun0503@gmail.com>
2025-09-27 15:59:57 +0900

    [level 2] Title: 귤 고르기, Time: 72.63 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -1,17 +1,20 @@
 import java.util.*;
 class Solution {
     public int solution(int k, int[] tangerine) {
-        int[] size = new int[10000001];
-        for(int i=0; i<tangerine.length; i++){
-            size[tangerine[i]] +=1;
+        int answer = 0;
+        HashMap<Integer, Integer> hash = new HashMap<>();
+        
+        for(int t : tangerine){
+            hash.put(t, hash.getOrDefault(t, 0) + 1);
         }
-        Integer[] ssize = Arrays.stream(size).boxed().toArray(Integer[]::new);
-        Arrays.sort(ssize, Collections.reverseOrder());
-        int various = 0;
-        while(k>0){
-            k -= ssize[various];
-            various++;
+        
+        List<Integer> list = new ArrayList<>(hash.keySet());
+        list.sort((o1, o2) -> hash.get(o2) - hash.get(o1));
+        for(int key: list){
+            k -=hash.get(key);
+            answer++;
+            if(k<=0) break;
         }
-        return various;
+        return answer;
     }
 }

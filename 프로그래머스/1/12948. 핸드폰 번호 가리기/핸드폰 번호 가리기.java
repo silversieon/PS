ed14044448fc7683e80364dd131b05ce27127f99
Sieon Keum <keumsiun0503@gmail.com>
2025-10-11 23:45:07 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.03 ms, Memory: 86.9 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -1,9 +1,9 @@
 class Solution {
     public String solution(String phone_number) {
-        String answer = "";
-        for(int i=0; i<phone_number.length()-4; i++){
-            answer +="*";
+        char[] ch = phone_number.toCharArray();
+        for(int i=0; i<ch.length-4; i++){
+            ch[i] = '*';
         }
-        return answer+phone_number.substring(phone_number.length()-4, phone_number.length());
+        return String.valueOf(ch);
     }
 }

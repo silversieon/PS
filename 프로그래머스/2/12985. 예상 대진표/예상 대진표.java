25ba794c8995431f66e4b7749ffa5f330a9d3c69
Sieon Keum <keumsiun0503@gmail.com>
2025-10-18 01:16:02 +0900

    [level 2] Title: 예상 대진표, Time: 0.34 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -0,0 +1,13 @@
+class Solution
+{
+    public int solution(int n, int a, int b)
+    {
+        int answer = 0;
+        while(a!=b){
+            answer++;
+            a = (int)Math.ceil(a/2.0);
+            b = (int)Math.ceil(b/2.0);
+        }
+        return answer;
+    }
+}

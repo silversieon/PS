b048262a1eba96c80d284a0e497f9ca48dd6e320
Sieon Keum <keumsiun0503@gmail.com>
2025-10-18 01:58:20 +0900

    [level 3] Title: 이중우선순위큐, Time: 53.64 ms, Memory: 114 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -2,32 +2,22 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] operations) {
-        int[] answer = new int[2];
-        TreeSet<Integer> ts = new TreeSet<>();
+        PriorityQueue<Integer> pqmin = new PriorityQueue<>();
+        PriorityQueue<Integer> pqmax = new PriorityQueue<>(Collections.reverseOrder());
         for(String operation : operations){
-            StringTokenizer st = new StringTokenizer(operation);
-            String command = st.nextToken();
-            if(command.equals("I")){
-                int a = Integer.parseInt(st.nextToken());
-                ts.add(a);
-            } else if(command.equals("D")) {
-                if(!ts.isEmpty()){
-                    String choice = st.nextToken();
-                    if(choice.equals("1")){
-                        ts.pollLast();
-                    } else if(choice.equals("-1")){
-                        ts.pollFirst();
-                    }
+            if(operation.startsWith("I ")){
+                int num = Integer.parseInt(operation.substring(2));
+                pqmin.add(num);
+                pqmax.add(num);
+            } else {
+                if(operation.equals("D 1")){
+                    pqmin.remove(pqmax.poll());
+                } else if(operation.equals("D -1")){
+                    pqmax.remove(pqmin.poll());
                 }
             }
         }
-        if(!ts.isEmpty()){
-            answer[0] = ts.last();
-            answer[1] = ts.first();
-        } else {
-            answer[0] = 0;
-            answer[1] = 0;
-        }
-        return answer;
+        if(pqmin.isEmpty() && pqmax.isEmpty()) return new int[]{0, 0};
+        return new int[]{pqmax.poll(), pqmin.poll()};
     }
 }

bf07a465cea05023530c046eb5fa0debd65e205a
Sieon Keum <keumsiun0503@gmail.com>
2025-11-02 12:13:13 +0900

    [level 3] Title: 단어 변환, Time: 0.45 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    private static boolean[] visited;
+    private static int answer;
+    public int solution(String begin, String target, String[] words) {
+        visited = new boolean[words.length];
+        answer = 0;
+        
+        dfs(begin, target, words, 0);
+        return answer;
+    }
+    
+    private void dfs(String begin, String target, String[] words, int cnt){
+        if(begin.equals(target)){
+            answer = cnt;
+            return;
+        }
+        for(int i=0; i<words.length; i++){
+            if(visited[i]) continue;
+            int sameCnt = 0;
+            for(int j=0; j<begin.length(); j++){
+                if(begin.charAt(j) == words[i].charAt(j)){
+                    sameCnt++;
+                }
+            }
+            if(sameCnt == begin.length()-1){
+                visited[i] = true;
+                dfs(words[i], target, words, cnt + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}

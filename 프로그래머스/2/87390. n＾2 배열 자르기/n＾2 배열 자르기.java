7ab6297061e506dd9fe1b0ccaa6ec3a30593a349
Sieon Keum <keumsiun0503@gmail.com>
2025-11-06 17:40:42 +0900

    Optimize solution method for n^2 array slicing

--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
@@ -1,18 +1,12 @@
 class Solution {
     public int[] solution(int n, long left, long right) {
         int[] answer = new int[(int)(right-left) + 1];
-        int[] arr = new int[n];
         int idx = 0;
-        int cnt = 0;
-        boolean isClear = false;
-        for(int i=0; i<n; i++){
-            arr[i] = i+1;
-        }
         for(long i=left; i<=right; i++){
             int quot = (int)Math.floor(i/n);
             int remain = (int)(i%n);
             if(remain>=quot){
-                answer[idx] = arr[remain];
+                answer[idx] = remain + 1;
             } else {
                 answer[idx] = quot + 1;
             }

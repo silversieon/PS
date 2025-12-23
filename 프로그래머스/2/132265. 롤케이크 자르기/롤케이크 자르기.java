feb0d6dec99d02179cca0b368d7e349feffc4ea7
Sieon Keum <keumsiun0503@gmail.com>
2025-12-24 01:38:19 +0900

    [level 2] Title: 롤케이크 자르기, Time: 238.58 ms, Memory: 192 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -7,9 +7,9 @@ class Solution {
         for(int i=0; i<topping.length; i++) b.put(topping[i], b.getOrDefault(topping[i], 0) + 1);
         for(int i=0; i<topping.length-1; i++){
             if(b.get(topping[i]) != 0) b.put(topping[i], b.get(topping[i]) - 1);
-            a.add(topping[i]);
             if(b.get(topping[i]) == 0) b.remove(topping[i]);
-            answer += a.size() == b.size() ? 1 : 0;
+            a.add(topping[i]);
+            answer += a.size() == b.keySet().size() ? 1 : 0;
         }
         return answer;
     }

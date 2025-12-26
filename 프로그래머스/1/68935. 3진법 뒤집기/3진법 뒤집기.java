00ae3b2b05da5df3b9e5b5a0de84a59174ced5fb
Sieon Keum <keumsiun0503@gmail.com>
2025-12-26 21:20:10 +0900

    [level 1] Title: 3진법 뒤집기, Time: 11.41 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,14 +1,10 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
         String result = "";
         while(n>0){
-            result += String.valueOf(n%3);
+            result += n % 3;
             n /= 3;
         }
-        for(int i=0; i<result.length(); i++){
-            answer += Integer.parseInt(result.substring(i, i+1)) * Math.pow(3, result.length() - (i + 1));
-        }
-        return answer;
+        return Integer.parseInt(result, 3);
     }
 }

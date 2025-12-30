90000414895b1ad6bd0148c56262f8bbf9b2367b
Sieon Keum <keumsiun0503@gmail.com>
2025-12-31 04:56:48 +0900

    [level 2] Title: 모음 사전, Time: 0.18 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,33 +1,22 @@
+import java.util.*;
 class Solution {
+    private static char[] gathers = {'A', 'E', 'I', 'O', 'U'};
+    public static int geometricSum(int a, int r, int n){
+        if(r==1) return a * n;
+        return a * ((int)Math.pow(r, n) - 1) / (r - 1);
+    }
     public int solution(String word) {
-        int answer = 0;
+        int answer = word.length();     
+        Map<Character, Integer> dic = new HashMap<>();
+        for(int i=0; i<gathers.length; i++){
+            dic.put(gathers[i], i);
+        }
+        
         for(int i=0; i<word.length(); i++){
             char gather = word.charAt(i);
-            int mulValue = 0;
-            switch(gather){
-                case 'A':
-                    mulValue = 0;
-                    break;
-                case 'E':
-                    mulValue = 1;
-                    break;
-                case 'I':
-                    mulValue = 2;
-                    break;
-                case 'O':
-                    mulValue = 3;
-                    break;
-                case 'U':
-                    mulValue = 4;
-                    break;
-            }
-            int sum = 0;
-            for(int j=i; j<5; j++){
-                sum += (int)(Math.pow(5, 5 - (j+1)));
-            }
-            answer += sum * mulValue;
+            int mulValue = dic.get(gather);
+            answer += geometricSum(1, 5, 5 - i) * mulValue;
         }
-        answer += word.length();
         return answer;
     }
 }

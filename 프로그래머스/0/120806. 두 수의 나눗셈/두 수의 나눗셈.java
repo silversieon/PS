8fdbb83dfdcc28fc61a61e02426942237ad179a4
Sieon Keum <keumsiun0503@gmail.com>
2026-01-04 18:59:15 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.02 ms, Memory: 81.6 MB -BaekjoonHub

--- 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -1,6 +1,5 @@
 class Solution {
     public int solution(int num1, int num2) {
-        double d = (double)num1/(double)num2;
-        return (int)(d*1000);
+        return num1 * 1000 / num2;
     }
 }

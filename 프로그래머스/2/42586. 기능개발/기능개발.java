2a6c8b06e2990161ef76058cfeb48b764609a0fa
Sieon Keum <keumsiun0503@gmail.com>
2026-01-05 14:32:57 +0900

    [level 2] Title: 기능개발, Time: 0.51 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        Queue<Integer> finishDay = new LinkedList<>();
+        for(int i=0; i<speeds.length; i++){
+            finishDay.offer((int)(Math.ceil((100 - progresses[i]) / (double)speeds[i])));
+        }
+        List<Integer> list = new ArrayList<>();
+        while(!finishDay.isEmpty()){
+            int progress = finishDay.peek();
+            int v = 0;
+            while(!finishDay.isEmpty() && progress>= finishDay.peek()){
+                finishDay.poll();
+                v++;
+            }
+            list.add(v);
+        }
+        int[] answer = new int[list.size()];
+        for(int i=0; i<list.size(); i++){
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+}

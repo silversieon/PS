b4c69ecf1ece1ec47530ee4a9f42f0db0fdc4c8c
Sieon Keum <keumsiun0503@gmail.com>
2026-01-05 14:53:23 +0900

    [level 2] Title: 기능개발, Time: 18.41 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -15,10 +15,6 @@ class Solution {
             }
             list.add(v);
         }
-        int[] answer = new int[list.size()];
-        for(int i=0; i<list.size(); i++){
-            answer[i] = list.get(i);
-        }
-        return answer;
+        return list.stream().mapToInt(a -> a).toArray();
     }
 }

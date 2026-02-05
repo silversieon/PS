1074ae60a47c5bf38b53402f82d7ffbb152de922
Sieon Keum <keumsiun0503@gmail.com>
2026-02-05 14:02:32 +0900

    [level 2] Title: 더 맵게, Time: 1398.16 ms, Memory: 128 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int item : scoville){
+            pq.add(item);
+        }
+        while(pq.peek() < K){
+            if(pq.size() == 1) return -1;
+            pq.add(pq.poll() + pq.poll() * 2);
+            answer++;
+        }
+        return answer;
+    }
+}

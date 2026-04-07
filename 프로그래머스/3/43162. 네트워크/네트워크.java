6c666bd2b22fadea1b41974ee1ab155d6abaa561
Sieon Keum <keumsiun0503@gmail.com>
2026-04-07 16:07:47 +0900

    [level 3] Title: 네트워크, Time: 0.15 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,25 +1,22 @@
 import java.util.*;
 class Solution {
-    static boolean[] visited;
-    static List<List<Integer>> graph = new ArrayList<>();
-    static void dfs(int node){
+    int n;
+    boolean[] visited;
+    int[][] computers;
+    
+    void dfs(int node){
         visited[node] = true;
-        for(int i=0; i<graph.get(node).size(); i++) {
-            int next = graph.get(node).get(i);
-            if(!visited[i] && next==1) dfs(i);
+        for(int i=0; i<n; i++) {
+            if (!visited[i] && computers[node][i] == 1) {
+                dfs(i);
+            }
         }
     }
     public int solution(int n, int[][] computers) {
-        int answer = 0;
+        this.n = n;
+        this.computers = computers;
         visited = new boolean[n];
-        for(int i=0; i<n; i++){
-            graph.add(new ArrayList<>());
-        }
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                graph.get(i).add(computers[i][j]);
-            }
-        }
+        int answer = 0;
         for(int i=0; i<n; i++){
             if(!visited[i]) {
                 dfs(i);

7d6ceedd21e701fe08074b07e9c95fc26fb3848c
Sieon Keum <keumsiun0503@gmail.com>
2026-09-22 12:11:22 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 0.03 ms, Memory: 84.5 MB -BaekjoonHub

--- 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
+++ 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
@@ -2,9 +2,6 @@ import java.util.*;
 
 class Solution {
     public String solution(String my_string) {
-        StringBuilder sb = new StringBuilder();
-        sb.append(my_string);
-        sb.reverse();
-        return sb.toString();
+        return new StringBuilder(my_string).reverse().toString();
     }
 }

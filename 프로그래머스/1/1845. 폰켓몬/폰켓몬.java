0e489e96eb52ee3e225d8989138032c5e3ba11df
Sieon Keum <keumsiun0503@gmail.com>
2026-09-23 22:03:06 +0900

    [level 1] Title: 폰켓몬, Time: 2.28 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -5,13 +5,7 @@ class Solution {
         // 1. nums 배열에 담긴 수를 HashSet에 put
         // 2. HashSet의 크기 size와 N/2를 비교, N/2 > size라면 size가 최대값, 그렇지 않다면 N/2가 최대값
         Set<Integer> set = new HashSet<>();
-        for(int num : nums) {
-            set.add(num);
-        }
-        if(set.size() > nums.length/2) {
-            return nums.length/2;
-        } else {
-            return set.size();
-        }
+        for(int num : nums) set.add(num);
+        return Math.min(set.size(), nums.length/2);
     }
 }

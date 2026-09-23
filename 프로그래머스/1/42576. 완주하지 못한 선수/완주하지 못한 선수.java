930feedb374db8b8bb0932186adb7f1b78d72a69
Sieon Keum <keumsiun0503@gmail.com>
2026-09-23 21:53:25 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 54.69 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -5,18 +5,16 @@ class Solution {
         //1. participant 순회, HashMap에 +1 (동명이인이 있을 수 있으니 카운트)
         //2. completion 순회, HashMap의 value에서 -1
         //3. participant 순회, value가 1인 키 반환
+        String answer = "";
         Map<String, Integer> map = new HashMap<>();
-        for(String part : participant) {
-            map.put(part, map.getOrDefault(part, 0) + 1);
-        }
-        for(String comp : completion) {
-            map.put(comp, map.get(comp) -1);
-        }
-        for(String part : participant) {
-            if(map.get(part) != 0) {
-                return part;
+        for(String player : participant) map.put(player, map.getOrDefault(player, 0) + 1);
+        for(String player : completion) map.put(player, map.get(player) -1);
+        for(String key : map.keySet()) {
+            if(map.get(key) != 0) {
+                answer = key;
+                break;
             }
         }
-        return "";
+        return answer;
     }
 }

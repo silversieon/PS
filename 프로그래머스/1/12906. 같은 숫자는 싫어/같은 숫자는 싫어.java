7c2b7520cac4f5df18ae6bc213255861492afbae
Sieon Keum <keumsiun0503@gmail.com>
2026-09-24 12:06:21 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 17.67 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -2,17 +2,18 @@ import java.util.*;
 
 public class Solution {
     public int[] solution(int []arr) {
-        // 1. arr을 순회하며 Stack 자료구조에 삽입
-        // 2. 만약 삽입 전에 stack.peek()와 숫자가 같다면 continue
-        // 3. Stack을 순회하며 크기만큼 배열에 넣고 반환
-        Stack<Integer> stack = new Stack<>();
+        // 1. arr을 순회하며 ArrayList 자료구조에 삽입
+        // 2. 만약 삽입 전 값이 이전 값과 같다면 삽입X
+        // 3. ArrayList를 int 배열에 담아 반환
+        List<Integer> list = new ArrayList<>();
+        int preNum = 10;
         for(int num : arr) {
-            if(!stack.isEmpty() && stack.peek() == num) continue;
-            stack.push(num);
+            if (preNum != num) list.add(num);
+            preNum = num;
         }
-        int[] answer = new int[stack.size()];
-        for(int i=stack.size()-1; i>=0; i--){
-            answer[i] = stack.pop();
+        int[] answer = new int[list.size()];
+        for(int i=0; i<list.size(); i++){
+            answer[i] = list.get(i);
         }
         return answer;
     }

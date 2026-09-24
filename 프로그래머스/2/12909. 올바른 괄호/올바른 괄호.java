5aac87e6143e7d7b9688906c9620a995691e13ac
Sieon Keum <keumsiun0503@gmail.com>
2026-09-24 13:25:07 +0900

    [level 2] Title: 올바른 괄호, Time: 10.42 ms, Memory: 57.2 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,22 +1,18 @@
+import java.util.*;
+
 class Solution {
     boolean solution(String s) {
-        boolean answer = true;
-        int openStack = 0;
+        boolean isCorrect = false;
+        Stack<Character> stack = new Stack<>();
         for(int i=0; i<s.length(); i++){
-            String bracket = s.substring(i, i+1);
-            if(bracket.equals("(")){
-                openStack++;
-            } else {
-                openStack--;
+            char c = s.charAt(i);
+            if(!stack.isEmpty() && c == ')' && stack.peek() == '(') {
+                stack.pop();
+                continue;
             }
-            if(openStack<0){
-                answer = false;
-                break;
-            }
+            stack.push(c);
         }
-        if(openStack != 0){
-            answer = false;
-        }
-        return answer;
+        if(stack.isEmpty()) isCorrect = true;
+        return isCorrect;
     }
 }

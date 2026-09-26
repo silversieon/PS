6cb170991e3ceb78c40193ff405727dbcc88917c
Sieon Keum <keumsiun0503@gmail.com>
2026-09-26 18:21:17 +0900

    [level 2] Title: 더 맵게, Time: 687.40 ms, Memory: 139 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/README.md
+++ 프로그래머스/2/42626. 더 맵게/README.md
@@ -1,10 +1,10 @@
 # [level 2] 더 맵게 - 42626 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626) 
 
 ### 성능 요약
 
-메모리: 128 MB, 시간: 1713.89 ms
+메모리: 139 MB, 시간: 687.40 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 05일 14:10:39
+2026년 09월 26일 18:21:14
 
 ### 문제 설명
 
--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,14 +1,24 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[] scoville, int K) {
-        int answer = 0;
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for(int item : scoville) pq.add(item);
-        while(pq.peek() < K){
-            if(pq.size() == 1) return -1;
-            pq.add(pq.poll() + pq.poll() * 2);
+    // 1. scoville 값들을 모두 우선순위큐에 넣는다.
+    // 2. 가장 낮은 scoville 값을 꺼내고, 7보다 작다면 섞는다.
+    // 3. 다시 우선순위 큐에 넣고 위 과정을 반복한다.
+    int answer = 0;
+    PriorityQueue<Integer> pq = new PriorityQueue<>();
+    for (int v : scoville) {
+        pq.add(v);
+    }
+    while(!pq.isEmpty()) {
+        int first = pq.poll();
+        if (first < K) {
+            if (pq.isEmpty()) return -1;
+            int second = pq.poll();
+            pq.add(first + second * 2);
             answer++;
         }
-        return answer;
+    }
+    return answer;
     }
 }
